73bf3fb14ed260bcd5c3b5d2041545c442cb5b7b
bahatiphill <gur79tub@gmail.com>
2020-03-02 22:07:53 +0200

    initial commit

---
+++ netot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NetotConfig(AppConfig):
+    name = 'netot'
---
+++ netot/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class RetailerOwner(models.Model):
+    pass
+
+
+class Gas(models.Model):
+    pass
+
+class Retailer(models.Model):
+    pass
+
+
+class Household(models.Model):
+    pass
+
+
